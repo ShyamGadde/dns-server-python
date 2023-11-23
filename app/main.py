88970f15b13c89aa74a95c467bdd01cfc1c0397a
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-11-24 04:08:23 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,45 @@
 import socket
+import struct
+from dataclasses import dataclass
+
+
+@dataclass
+class DNSHeader:
+    id: int
+    qr: int
+    opcode: int
+    aa: int
+    tc: int
+    rd: int
+    ra: int
+    z: int
+    rcode: int
+    qdcount: int
+    ancount: int
+    nscount: int
+    arcount: int
+
+
+def pack_dns_message(message: DNSHeader) -> bytes:
+    flags = (
+        (message.qr << 15)
+        | (message.opcode << 11)
+        | (message.aa << 10)
+        | (message.tc << 9)
+        | (message.rd << 8)
+        | (message.ra << 7)
+        | (message.z << 4)
+        | message.rcode
+    )
+    return struct.pack(
+        ">HHHHHH",
+        message.id,
+        flags,
+        message.qdcount,
+        message.ancount,
+        message.nscount,
+        message.arcount,
+    )
 
 
 def main():
@@ -10,6 +51,8 @@ def main():
             buf, source = udp_socket.recvfrom(512)
 
             response = b""
+            header = DNSHeader(1234, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+            response += pack_dns_message(header)
 
             udp_socket.sendto(response, source)
         except Exception as e:
