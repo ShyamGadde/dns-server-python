ef221672b189235915063e87a4dc5f80b4689ff1
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-11-24 18:17:34 +0530

    pass 8th stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import argparse
-import binascii
 import socket
 import struct
 from dataclasses import dataclass
@@ -195,11 +194,11 @@ class DNSResponse:
             query.header.qdcount = 1
             for question in query.questions:
                 query_data = query.header.pack() + question.pack()
-                print("Sending query:", binascii.hexlify(query_data))
+                print("Sending query:", query_data)
                 dns_resolver.sendto(query_data, (ip, port))
 
                 dns_resolver_response, _ = dns_resolver.recvfrom(512)
-                print("Received response:", binascii.hexlify(dns_resolver_response))
+                print("Received response:", dns_resolver_response)
                 # We are skipping 4 bytes for the type and class fields
                 answer_offset = dns_resolver_response.index(b"\x00", 12) + 5
                 print("Answer offset:", answer_offset)
